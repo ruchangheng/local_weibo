258efb68ca2685ee7b975d92867109355613f34c
ruchangheng <624813409@qq.com>
2020-08-17 06:39:18 +0000

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->times(100)->create();
+
+        $user = User::find(1);
+        $user->name = '茹长恒';
+        $user->email = '624813409@qq.com';
+        $user->save();
+    }
+}
